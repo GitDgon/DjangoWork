db95c55fab8de50684626f8739c839ae778d5555
GitDgon <dgon_mail@mail.ru>
2022-11-12 09:02:10 +0300

    создание проекта ДЗ

---
+++ MainCountries/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
+
+# Create your views here.
+
+author = {
+    "name": "Андрей",
+    "surname": "Атом",
+    "midlename": "Андреевич",
+}
